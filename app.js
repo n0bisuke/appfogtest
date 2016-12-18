80ce07aa666197b41998996cb31ceb511aa66e2e
のびすけ <rsksound@gmail.com>
2016-12-18 16:14:17 +0900

    push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var port = process.env.VCAP_APP_PORT || 3000;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(8124);
+}).listen(port);
